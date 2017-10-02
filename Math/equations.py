f1db752db7567bda2bf08275a1da9c7c533094ed
pyMurphy <32463165+pyMurphy@users.noreply.github.com>
2017-10-02 18:47:07 +0100

    Update equations.py

--- Math/equations.py
+++ Math/equations.py
@@ -6,13 +6,13 @@ space_list = [' ','+','-','/','*','=']		# List of possible characters used in be
 
 INVALID_EQUATION = 'Invalid Equation'		# Global string returned when the equation is invalid
 
-def is_equation(equation, xpos):		# Returns whether an equation is valid or not
+def is_equation(equation, xpos):			# Returns whether an equation is valid or not
 	if xpos != -1 and '=' in equation:		# If the equation has a variable and has an equals sign then it is a valid equation
 		return True
 	else:									# If not, it is invalid
 		return False
 
-def find_answer(equation):			# Finds the answer to the equation
+def find_answer(equation):					# Finds the answer to the equation
 	eqn = equation.split('=')				# Splits string into two parts before and after equals sign
 	for var in var_list:					# Loops through ever character in the variable list
 		if var in eqn[0]:					# If a variable is in the left hand side of the equation
@@ -20,20 +20,20 @@ def find_answer(equation):			# Finds the answer to the equation
 		elif var in eqn[1]:					# Similarly, if a variable is in the right hand side of the equation
 			return int(eqn[0].lstrip())		# then it will return the left hand side of the equation as the answer
 
-def check_negative(equation, xpos, value):		# Checks if the coefficient is negative or positive
-	xlen = len(equation[:xpos])			# Finds amount of characters before the variable
-	for pos in range(xlen,-1,-1):		# Loops from the character position and goes backwards
-		if '-' in equation[pos]:		# If the character at that position is equal to '-' then we return the negative of the value
+def check_negative(equation, xpos, value):	# Checks if the coefficient is negative or positive
+	xlen = len(equation[:xpos])				# Finds amount of characters before the variable
+	for pos in range(xlen,-1,-1):			# Loops from the character position and goes backwards
+		if '-' in equation[pos]:			# If the character at that position is equal to '-' then we return the negative of the value
 			return -value
-		if '+' in equation[pos]:		# If the character at that position is equal to '+' then we return the value as normal as it is positive
+		if '+' in equation[pos]:			# If the character at that position is equal to '+' then we return the value as normal as it is positive
 			return value
-	return value 				# If there are no characters before the number we assume it is already positive
+	return value 							# If there are no characters before the number we assume it is already positive
 
-def check_coefficient(equation, xpos):	# Returns the coefficient of the variable
+def check_coefficient(equation, xpos):					# Returns the coefficient of the variable
 	result=[]		
-	chars_before = len(equation[:xpos])				# Gets the number of characters before the position of the variable in the string
+	chars_before = len(equation[:xpos])					# Gets the number of characters before the position of the variable in the string
 	spaced = False
-	while not spaced and xpos > 0:					# Loops until every character before is checked or there are no more digits in the coefficient
+	while not spaced and xpos > 0:						# Loops until every character before is checked or there are no more digits in the coefficient
 		for coefficient in coefficient_list:		
 			if str(coefficient) in equation[xpos-1]:	# Checks if the digit 0-9 is equal to the character
 				result.append(coefficient)				# adds digit to the results array
@@ -47,28 +47,28 @@ def check_coefficient(equation, xpos):	# Returns the coefficient of the variable
 		coefficient_str = ''.join(coefficient_list_str)				# Joins items in the results array into a single string
 		return int(coefficient_str)									# Turns string into integer and returns the coefficient of the variable
 	except:
-		return 1										# if there are no coefficients, we assume it is being multiplied by 1
+		return 1													# if there are no coefficients, we assume it is being multiplied by 1
 
-def find_var(equation, var):	# Returns the true value of the coefficient and handles errors
-	equation = equation.lower()			# Converts the equation to lowercase to check for
+def find_var(equation, var):						# Returns the true value of the coefficient and handles errors
+	equation = equation.lower()						# Converts the equation to lowercase to check for
 	try:
 		xpos = equation.find(var)					# Finds the position of the variable passed into the function
 		if is_equation(equation,xpos):				# Checks the equation is a valid equation
 			c = check_coefficient(equation,xpos)	# Stores the coefficient of the variable in 'c'
 			return check_negative(equation,xpos, c) # returns the negative or positive value of the coefficient
 		else:
-			return INVALID_EQUATION 			# Returns INVALID_EQUATION string if the equation is invalid
+			return INVALID_EQUATION 				# Returns INVALID_EQUATION string if the equation is invalid
 	except:
-		return 'Error'				# Returns 'Error' string if something went wrong
+		return 'Error'								# Returns 'Error' string if something went wrong
 
-def equation(e, var):		# Solves the equation for the variable inputted
+def equation(e, var):							# Solves the equation for the variable inputted
 	try:
-		c = find_var(e, var)				# Returns the value of the coefficient
-		answer = find_answer(e)				# Returns the value the equation equals
-		eqn_answer = answer/c 				# Divides the answer by the coefficient to solve for the variable
-		return var+' = '+str(eqn_answer)	# Returns "variable = answer"
+		c = find_var(e, var)					# Returns the value of the coefficient
+		answer = find_answer(e)					# Returns the value the equation equals
+		eqn_answer = answer/c 					# Divides the answer by the coefficient to solve for the variable
+		return var+' = '+str(eqn_answer)		# Returns "variable = answer"
 	except:
-		if find_var(e,var) == INVALID_EQUATION:		# Checks if find_var returns INVALID_EQUATION
-			return INVALID_EQUATION					# Returns INVALID_EQUATION if it does
+		if find_var(e,var) == INVALID_EQUATION:	# Checks if find_var returns INVALID_EQUATION
+			return INVALID_EQUATION				# Returns INVALID_EQUATION if it does
 		else:
-			return 'Error'				# If the error is not to do with it being an invalid equation, it returns 'Error'
+			return 'Error'						# If the error is not to do with it being an invalid equation, it returns 'Error'
